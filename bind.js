9834e8f04ff346ed7139d4cf684f7c2703fbbc5e
▲M▲N <51995611+amanbhoria@users.noreply.github.com>
2025-01-16 01:02:00 +0530

    Update bind.js

--- bind.js
+++ bind.js
@@ -2,3 +2,15 @@
 
 // call will directly call the function
 // whereas bind will return the function
+
+const radha = {
+name: "radha", sayName: function () { console.log(this.name);
+},
+const aman = {
+name: "Aman Bhoria", sayName: function () { console.log(this.name);
+},
+
+aman.SayName.call(radha); 
+const newFun = aman.SayName.bind(radha);
+
+// I can call newFun as many times I want 
